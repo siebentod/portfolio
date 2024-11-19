5471f3066af2f55c9fbb2331d05572dd59c626c1
siebentod <33163165+siebentod@users.noreply.github.com>
2024-11-19 12:52:41 +0300

    1.0

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,3 @@
-// This is the root layout component for your Next.js app.
-// Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
-// import { cn } from '@/lib/utils';
 import './_styles/globals.css';
 import './_styles/fonts.css';
 
