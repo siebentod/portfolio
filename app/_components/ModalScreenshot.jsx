b6898fd1c6f539c8f36812226cdac41893153161
siebentod <33163165+siebentod@users.noreply.github.com>
2025-01-24 16:03:17 +0300

    smallfix

--- app/_components/ModalScreenshot.jsx
+++ app/_components/ModalScreenshot.jsx
@@ -5,7 +5,7 @@ function ModalScreenshot({ openedModal, closeModal }) {
         className="relative bg-[#242424] m-3 rounded-lg shadow-lg  grid justify-center items-center z-10 animate-slide-up cursor-pointer select-none"
         onClick={closeModal}
       >
-        <img src={openedModal.src} alt="Project 1" />
+        <img src={openedModal.src} alt="Project 1" className="max-h-[95vh]" />
         {/* <button
           onClick={closeModal}
           className="absolute top-4 right-4 text-white"
